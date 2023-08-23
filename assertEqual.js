d9aa0f485814c92c3bdf2952597ea6e5648df9f3
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-08-22 21:09:06 -0600

    assertion function which compares two values and prints out a  success or fail message.

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+
+// FUNCTION IMPLEENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual("That's a spicy meatball", "That's a spicy meatball");
+assertEqual(1, 1);
+assertEqual(42, 1991);
