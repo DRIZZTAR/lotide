5883f745d35ccfda24dd2b6e398a2dde60a652a4
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-08-26 14:42:18 -0600

    Change assertEqual to check for === arrays

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const eqArrays = function(actual, expected) {
+  // CHeck if arrays have same length, return false if !==
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  //Loop through elements in arrays to see if they !==
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  //Because our arrays have the same length and we've checked their elements by looping through, they will be === and true can be returned
+  return true;
+};  
+
+// TEST CODE
+console.log(eqArrays([42, 42, 42], [42, 42, 42])); // true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // false
+console.log(eqArrays(["Batman", "Spider-man", "Hulk"], ["Batman", "Green Goblin", "Hulk"])); // false
+console.log(eqArrays(["Batman", "Spider-man", "Hulk"], ["Batman", "Spider-man", "Hulk"])); // true
