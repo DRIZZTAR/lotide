a77cac704117c76a8a2b80d6668a4fd359ad4e1a
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-08-28 13:47:53 -0600

    added comments, and linted. allItems, and itemsToCount were not defined

--- countOnly.js
+++ countOnly.js
@@ -11,30 +11,33 @@ const assertEqual = function(actual, expected) {
 const countOnly = function(allItems, itemsToCount) {
   //Create empty object to store new key, and value pairs
   const results = {};
-
+  //loop through all elements in allItems
   for (const item of allItems) {
-    if (itemsToCount[item]) { 
+    //check if found element is in itemsToCount
+    if (itemsToCount[item]) {
+      //if item is in results as a key, add 1 to its value, the else staement will catch if item is not a key in results
       if (results[item]) {
         results[item] += 1;
       } else {
+        //if item is not in results as a key initialize a value of one
         results[item] = 1;
       }
     }
   }
 
-  // Return the results object
+  // Return the results (key, and balue pairs) to the empty results object
   return results;
 };
 
 //allItems: an array of strings that we need to look through
-allItems = ["a", "a", "b", "c", "d", "e", "a"]
+const allItems = ["a", "a", "b", "c", "d", "e", "a"];
 //itemsToCount: an object specifying what to count
-itemsToCount = {
+const itemsToCount = {
   a: true,
   d: true,
   b: false,
   f: true
-}
+};
 
 const results = countOnly(allItems, itemsToCount);
 console.log(results);
