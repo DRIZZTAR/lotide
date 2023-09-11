65b6785b11b196ebfe91a7fcfc95eb2f629b2655
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-11 12:50:49 -0600

    refactor assertArraysEqual, and add test to seperate file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,12 @@
+const eqArrays = require('./eqArrays'); // Import your eqArrays function
+
 const assertArraysEqual = function(actual, expected) {
-  // Check if arrays have same length, return false if !==
-  if (actual.length !== expected.length) {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-    return;
+  // Use my eqArrays function to compare the arrays
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: [${actual}] !== [${expected}]`);
   }
-  // Loop through elements in arrays to see if they !==
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-      return;
-    }
-  }
-  // We now know that the arrays are equal. We can now console.log to output to the console
-  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
 };
 
-
-assertArraysEqual("Lighthouse Labs", "Bootcamp");
-assertArraysEqual("That's a spicy meatball", "That's a spicy meatball");
-assertArraysEqual(1, 1);
-assertArraysEqual(42, 1991);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,15 @@
+const assertArraysEqual = require('../assertArraysEqual')
+
+// Test cases for assertArraysEqual
+
+// Test case 1: Comparing two non-array strings (should fail)
+assertArraysEqual("Lighthouse Labs", "Bootcamp");
+
+// Test case 2: Comparing two non-array strings (should pass)
+assertArraysEqual("That's a spicy meatball", "That's a spicy meatball");
+
+// Test case 3: Comparing two arrays with equal content (should pass)
+assertArraysEqual([1], [1]);
+
+// Test case 4: Comparing two arrays with different content (should fail)
+assertArraysEqual([42], [1991]);
