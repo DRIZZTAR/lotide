bedc8e142ccf55922dae1d52cff24a30d31ca601
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-12 15:34:20 -0600

    refactoring countOnly, moved tests to test folder

--- countOnly.js
+++ countOnly.js
@@ -1,66 +1,20 @@
-// assertEqual function
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-//countOnly function
 const countOnly = function(allItems, itemsToCount) {
-  //Create empty object to store new key, and value pairs
-  const results = {};
-  //loop through all elements in allItems
+  const filteredItems = {};
+
   for (const item of allItems) {
-    //check if found element is in itemsToCount
     if (itemsToCount[item]) {
-      //if item is in results as a key, add 1 to its value, the else staement will catch if item is not a key in results
-      if (results[item]) {
-        results[item] += 1;
+      if (filteredItems[item]) {
+        filteredItems[item] += 1;
       } else {
-        //if item is not in results as a key initialize a value of one
-        results[item] = 1;
+        filteredItems[item] = 1;
       }
     }
   }
 
-  // Return the results (key, and balue pairs) to the empty results object
-  return results;
+  return filteredItems;
 };
 
-//allItems: an array of strings that we need to look through
-const allItems = ["a", "a", "b", "c", "d", "e", "a"];
-//itemsToCount: an object specifying what to count
-const itemsToCount = {
-  a: true,
-  d: true,
-  b: false,
-  f: true
-};
+module.exports = countOnly;
 
-const results = countOnly(allItems, itemsToCount);
-console.log(results);
+module.exports = countOnly;
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-//Checking if the count of Jason in firstNames is = to 1, should return true and pasds assertEqual
-assertEqual(result1["Jason"], 1);
-//checking if Karima is not in result 1, it passes because karima being undefned in firstNamesis true
-assertEqual(result1["Karima"], undefined);
-//CHecking if Fang is found 2 times in firstNames, assertEqual should pass and retrun true
-assertEqual(result1["Fang"], 2);
-//Checking if Agouhanna is !== to false in firstNames. It passes because Agouhanna being undefined is false
-assertEqual(result1["Agouhanna"], undefined);
---
+++ test/countOnlyTest.js
@@ -0,0 +1,66 @@
+const assert = require('chai').assert;
+const countOnly = require('../countOnly'); // Import your countOnly function
+
+// Test case 1: Count specific elements
+describe('countOnly', function() {
+  const allItems = ["a", "a", "b", "c", "d", "e", "a"];
+  const itemsToCount = {
+    a: true,
+    d: true,
+    b: false,
+    f: true
+  };
+
+  it('should return a specific elements based on the condition', function() {
+    const results = countOnly(allItems, itemsToCount);
+
+    assert.equal(results['a'], 3, 'Count of "a" should be 3');
+    assert.equal(results['d'], 1, 'Count of "d" should be 1');
+    assert.isUndefined(results['b'], 'Count of "b" should be undefined');
+    assert.isUndefined(results['f'], 'Count of "f" should be undefined');
+  });
+});
+
+// Test case 2: Check for items not in the result
+describe('countOnly', function() {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+
+  it('should return undefined for items not in the result', function() {
+    const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+    assert.isUndefined(result1['Karima'], 'Result for "Karima" should be undefined');
+    assert.isUndefined(result1['Agouhanna'], 'Result for "Agouhanna" should be undefined');
+  });
+});
+
+// Test case 3: Check counts for items in the result
+describe('countOnly', function() {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+
+  it('should return the correct counts for items in the result', function() {
+    const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+    assert.equal(result1['Jason'], 1, 'Count of "Jason" should be 1');
+    assert.equal(result1['Fang'], 2, 'Count of "Fang" should be 2');
+  });
+});
