d8104a95d2f38dbee34d30b0c3c3a4673efa83bd
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-13 00:22:08 -0600

    add clear comments to find key

--- findKey.js
+++ findKey.js
@@ -1,13 +1,14 @@
-//function findKey
-//Function takes in an object and callback function as parameters
+// The `findKey` function takes an object and a callback function as parameters.
 function findKey(obj, callback) {
-  //scan the objects keys
+  // Iterate through the keys of the object.
   for (let key in obj) {
-    //scan keys to see if they return a truthy value that returns a truthy value
+    // Check if the callback function returns a truthy value for the current key's value.
     if (callback(obj[key])) {
+      // If a truthy value is found, return the current key.
       return key;
     }
   }
+  // If no matching key is found, return undefined.
   return undefined;
 }
 
