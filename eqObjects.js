9e1d9e433c76245dad166a1eddc28a310720a3c6
Tyson Skakun <tyson_jeremy@hotmail.com>
2023-09-12 22:18:45 -0600

    refactor equal objects to account for nested objects

--- eqObjects.js
+++ eqObjects.js
@@ -1,24 +1,30 @@
-const eqObjects = function(object1, object2) {
-  const keys1 = Object.keys(object1).sort();
-  const keys2 = Object.keys(object2).sort();
+const eqObjects = function(obj1, obj2) {
+  // Get the keys of both objects
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
 
+  // Check if the number of keys is the same
   if (keys1.length !== keys2.length) {
     return false;
   }
 
+  // Iterate through the keys
   for (const key of keys1) {
-    const val1 = object1[key];
-    const val2 = object2[key];
+    const val1 = obj1[key];
+    const val2 = obj2[key];
 
+    // Check if both values are objects
     if (typeof val1 === 'object' && typeof val2 === 'object' && val1 !== null && val2 !== null) {
+      // Recursively check nested objects
       if (!eqObjects(val1, val2)) {
-        return false;
+        return false; // If nested objects are not equal, return false
       }
     } else if (val1 !== val2) {
-      return false;
+      return false; // If values are not equal, return false
     }
   }
 
+  // If all keys and values match, the objects are equal
   return true;
 };
 
